70ac3fd901f9dfab5e99f8cff306bc840acb9902
Brunna Serafina <brunnaserafina@gmail.com>
2023-05-06 12:00:14 -0300

    feat: creating add new list functionality

--- src/common/Lists.js
+++ src/common/Lists.js
@@ -4,12 +4,23 @@ import styled from "styled-components";
 
 export default function Lists() {
   const [selected, setSelected] = useState(0);
-
-  const items = ["Rotina", "Trabalho", "Faculdade", "Compras"];
+  const [createdNewList, setCreatedNewList] = useState(false);
+  const [titleList, setTitleList] = useState("");
+  const [items, setItems] = useState([]);
 
   const handleItemClick = (index) => {
     setSelected(index);
   };
+
+  function addNewList() {
+    if (titleList.length < 1) {
+      return;
+    }
+
+    setItems([...items, titleList]);
+    setTitleList("");
+    setCreatedNewList(false);
+  }
 
   return (
     <>
@@ -22,15 +33,70 @@ export default function Lists() {
         ))}
       </List>
 
-      <AddNewList>
-        <span>
-          <BsPlus fontSize={"28px"} />
-        </span>
-        <h2>Criar nova lista</h2>
-      </AddNewList>
+      {createdNewList ? (
+        <CreateNewList>
+          <input
+            type="text"
+            placeholder="Digite sua nova tarefa"
+            value={titleList}
+            onChange={(e) => setTitleList(e.target.value)}
+          />
+          <div>
+            <button onClick={() => setCreatedNewList(false)}>Cancelar</button>
+            <button onClick={addNewList}>Salvar</button>
+          </div>
+        </CreateNewList>
+      ) : (
+        <AddNewList onClick={() => setCreatedNewList(true)}>
+          <span>
+            <BsPlus fontSize={"28px"} />
+          </span>
+          <h2>Criar nova lista</h2>
+        </AddNewList>
+      )}
     </>
   );
 }
+
+const CreateNewList = styled.div`
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  margin-left: 28px;
+
+  div {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  input {
+    border-radius: 5px;
+    padding: 10px;
+    width: 100%;
+    height: 30px;
+  }
+
+  input:focus {
+    outline: 0;
+    border: solid 1.5px var(--light-green);
+    caret-color: var(--dark-green);
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+    background-color: var(--red);
+    color: var(--white);
+    border-radius: 5px;
+    height: 20px;
+    width: 80px;
+  }
+
+  button:nth-of-type(2) {
+    background-color: var(--light-green);
+  }
+`;
 
 const List = styled.ul`
   display: flex;
@@ -46,7 +112,7 @@ const List = styled.ul`
     width: 20px !important;
   }
 
-  li{
+  li {
     display: flex;
     align-items: center;
     height: fit-content;
@@ -55,6 +121,8 @@ const List = styled.ul`
 `;
 
 const AddNewList = styled.div`
+  cursor: pointer;
+
   span {
     width: 20px !important;
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,132 @@
 import Header from "../common/Header";
+import styled from "styled-components";
+import { RiDeleteBinLine } from "react-icons/ri";
+import { BsCheckLg } from "react-icons/bs";
+import { AiOutlinePlus } from "react-icons/ai";
+import { IoIosArrowDown } from "react-icons/io";
 
 export default function Home() {
+  const tasks = ["Ler 10 páginas", "Almoçar", "Trabalhar"];
   return (
     <>
-      <Header></Header>
+      <Header />
+      <WrapperHome>
+        <Tasks>
+          <span>
+            <h1>Rotina</h1>
+            <RiDeleteBinLine font-size={"20px"} color={"var(--dark-green)"} />
+          </span>
+
+          <ul>
+            {tasks.map((item, index) => (
+              <li key={index}>
+                <Check>
+                  <div>
+                    <BsCheckLg color={"white"} />
+                  </div>
+                </Check>
+                {item}
+              </li>
+            ))}
+          </ul>
+
+          <div>
+            <Check>
+              <AiOutlinePlus color={"white"} />
+            </Check>
+            <p>Adicionar tarefa</p>
+          </div>
+        </Tasks>
+
+        <TasksCompleted>
+          <span>
+            <h2>Tarefas concluídas (2)</h2>
+            <IoIosArrowDown cursor={"pointer"} />
+          </span>
+        </TasksCompleted>
+      </WrapperHome>
     </>
   );
 }
+
+const TasksCompleted = styled.div`
+  width: 22.5vw;
+
+  span {
+    padding-right: 2.5vw;
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+  }
+`;
+
+const Check = styled.div`
+  width: 25px;
+  height: 25px;
+  border-radius: 40px;
+  background-color: var(--light-green);
+  margin-right: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    display: none;
+  }
+
+  &:hover div {
+    display: initial;
+  }
+
+  &:hover<li {
+    text-decoration: line-through;
+  }
+`;
+
+const WrapperHome = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+`;
+
+const Tasks = styled.div`
+  width: 22.5vw;
+  min-height: 30vh;
+  height: fit-content;
+  padding-right: 2.5vw;
+
+  h1 {
+    font-weight: 700;
+    font-size: 18px;
+  }
+
+  span {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 5vh;
+    margin-bottom: 2vh;
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+    color: var(--dark-green);
+    cursor: pointer;
+  }
+
+  li:hover {
+    text-decoration: line-through;
+  }
+
+  p {
+    color: var(--dark-green);
+  }
+
+  > div {
+    display: flex;
+    align-items: center;
+    margin-top: 25px;
+  }
+`;
